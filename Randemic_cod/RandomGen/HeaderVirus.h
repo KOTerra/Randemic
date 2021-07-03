ac99e69647a364fc26ca938e7ee14b31a189f4ba
Tudosie Razvan <razvanmariustudosie25@gmail.com>
2021-07-03 21:59:07 +0300

    Adaugare clasa oras

--- Randemic_cod/RandomGen/HeaderVirus.h
+++ Randemic_cod/RandomGen/HeaderVirus.h
@@ -1,9 +1,9 @@
 #pragma once
 //tot felul de constante si variabile pentru virus si simulare
-int infectabilitate = 8;
-int timpRecuperare = 1000;
-const int timpIncubare = 0;
-int mortalitate = 20;
+int infectabilitate = 8; //procent din 100%
+int timpRecuperare = 1000;//frameuri
+const int timpIncubare = 0;//timp incubare
+int mortalitate = 20;//procent din 100%
 
 int counterMorti = 0;
 int counterVii;
